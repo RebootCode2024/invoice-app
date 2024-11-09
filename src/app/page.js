b1558b757294116dacc1900d48421fede5a4a99c
Harsh Bhojwani <rebootcode2024@gmail.com>
2024-11-09 08:46:36 +0530

    invoice working

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,31 @@
-import Image from "next/image";
+"use client"
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
+  const router = useRouter();
+
+  const navigateToInvoice = () => {
+    router.push('/invoice');
+  };
+
   return (
-        <main></main>
+    <main style={{ textAlign: 'center', paddingTop: '20px' }}>
+      <h1>Welcome to Geeta Footwear</h1>
+      <button
+        onClick={navigateToInvoice}
+        style={{
+          padding: '10px 20px',
+          fontSize: '16px',
+          backgroundColor: '#4CAF50',
+          color: 'white',
+          border: 'none',
+          borderRadius: '4px',
+          cursor: 'pointer',
+          marginTop: '20px',
+        }}
+      >
+        Invoice
+      </button>
+    </main>
   );
 }
